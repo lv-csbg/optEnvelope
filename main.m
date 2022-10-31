cbee22325284336fb08ca895e5d1c40f721c77ee
Kristaps Berzins <49905456+Kretaks@users.noreply.github.com>
2022-10-31 12:43:25 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,11 +1,23 @@
 clc
 clear
-% 
+% Choose solver
 changeCobraSolver('gurobi','all',0);
+
+% Load model
 model=readCbModel('iJR904');
+
+% Describe biomass
 biomass='BIOMASS_Ecoli';
+
+% Describe target product
 desiredProduct='EX_ac_e';
 desiredProductName='Acetate';
+prMolarMass = 60;
+
+% Convet model to irreversible form
 [model,matchRev] = convertToIrreversible(model);
+% Create a list of protected reactions (their IDs)
 K=[find(contains(model.rxns,'EX_')); findRxnIDs(model, 'H2Ot_f'); findRxnIDs(model, 'H2Ot_b')];
-knockouts = optEnvelope(model,biomass,desiredProduct,desiredProductName,matchRev,K, 60)
+
+%run optEnvelope
+knockouts = optEnvelope(model,biomass,desiredProduct,desiredProductName,matchRev,K, prMolarMass)
